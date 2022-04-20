936aa563d9e577696bdd3202d15f36ff8d0de2b7
SilverAndrels <andresilva64@outlook.com>
2022-04-20 14:23:30 -0300

    Fatorial Calculator

---
+++ fatorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// o fatorial de um numero inteiro n
+double fatorial(int n)
+{
+  double vfat;
+  if ( n <= 1 )
+     //Caso base: fatorial de n <= 1 retorna 1
+     return (1);
+  else
+  {
+     //Chamada recursiva
+     vfat = n * fatorial(n - 1);
+     return (vfat);
+  }
+}
+
+int main(void)
+{
+  int numero;
+  double f;
+  printf("Digite o numero que deseja calcular o fatorial: ");
+  scanf("%d",&numero);
+  f = fatorial(numero);
+  printf("Fatorial de %d = %.0lf",numero,f);
+  return 0;
+}
